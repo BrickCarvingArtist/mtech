ce3c2f41bafb1134392c6a822123169aa4b6066f
pengcheng.yang <806321554@qq.com>
2016-09-24 19:23:28 +0800

    debug

--- controller/static/index.js
+++ controller/static/index.js
@@ -5,7 +5,7 @@ const Corp = "砖雕艺术馆管理系统",
 	{
 		authHost
 	} = SessionConfig,
-	port = process.env.NODE_ENV === "DEVELOPMENT" ? `:${ServerConfig.nginxPort}` : "",
+	hostname = process.env.NODE_ENV === "DEVELOPMENT" ? `http://127.0.0.1:${ServerConfig.nginxPort}` : "http://manage.tech.ikindness.cn",
 	Enum = [
 		{
 			route : "*",
@@ -13,7 +13,7 @@ const Corp = "砖雕艺术馆管理系统",
 				if(req.session.user){
 					res.end(readFileSync(`${process.cwd()}/resource/index.html`, "utf-8"));
 				}else{
-					res.redirect(`${authHost}?host=http://${req.hostname}${port}&referer=${req.path}&auth=/api/auth/signIn`);
+					res.redirect(`${authHost}?host=${hostname}&referer=${req.path}&auth=/api/auth/signIn`);
 				}
 			}
 		}
